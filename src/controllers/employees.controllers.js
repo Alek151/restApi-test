abab700c28bf7c9da25b16c3e7c5e96175c1c08e
Alexander <bryangar266@gmail.com>
2022-11-21 09:33:27 -0600

    actualizandoQuerysFuncionales

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -19,7 +19,7 @@ export const obtenerEmpleado = async (req, res) => {
 export const crearEmpleado = async (req, res) => {
     const { name, salary } = req.body
     try {
-        const [rows] = await pool.query('INSERT INTO companydb.employee (name, salary) VALUES(?, ?)', [name, salary])
+        const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES(?, ?)', [name, salary])
         res.send({
             id: rows.insertId,
             name,
@@ -35,7 +35,7 @@ export const crearEmpleado = async (req, res) => {
 //obtener un único empleado
 export const obtenerEmpleadoUnico = async (req, res) => {
     try {
-        const [rows] = await pool.query('SELECT * FROM companydb.employee WHERE id = ?', [req.params.id])
+        const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
         if (rows.length <= 0) return res.status(404).json({
             message: 'Empleado no éxiste'
         })
@@ -52,11 +52,11 @@ export const actualizarEmpleado = async (req, res) => {
     const { id } = req.params;
     const { name, salary } = req.body;
     try {
-        const [result] = await pool.query('UPDATE companydb.employee SET name=ifnull(?, name) , salary=ifnull(?, salary)  WHERE id=?', [name, salary, id])
+        const [result] = await pool.query('UPDATE employee SET name=ifnull(?, name) , salary=ifnull(?, salary)  WHERE id=?', [name, salary, id])
         if (result.affectedRows === 0) return res.status(404).json({
             message: "No se encuentra el empleado"
         })
-        const [rows] = await pool.query('SELECT * FROM companydb.employee WHERE id=?', [id])
+        const [rows] = await pool.query('SELECT * FROM employee WHERE id=?', [id])
         res.json(rows[0])
     } catch (error) {
         return res.status(500).json('Algo salió mal')
@@ -66,7 +66,7 @@ export const actualizarEmpleado = async (req, res) => {
 //Ruta y proceso para la eliminación de empleado.
 export const eliminarEmpleado = async (req, res) => {
     try {
-        const [rows] = await pool.query('DELETE FROM companydb.employee WHERE id = ?', [req.params.id])
+        const [rows] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id])
         if (rows.affectedRows <= 0) return res.status(404).json({
             message: "Empleado no éxiste, no puedes eliminarlo"
         })
